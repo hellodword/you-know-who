607d535b9b5ee20427deea4c8830aa319d59a684
hellodword <46193371+hellodword@users.noreply.github.com>
2024-02-12 03:10:55 +0000

    Delete shadowrocket directory

--- shadowrocket/reading.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-curl -f -sSL https://raw.githubusercontent.com/shidahuilang/shuyuan/shuyuan/good.json | \
-    jq '.[] | .bookSourceUrl, .bookUrlPattern, .searchUrl' | \
-    grep -oP 'https?:\\?\/\\?\/\w+(\.\w+)+' | \
-    grep -oP '(?<=/)\w+(\.\w+)+' | \
-    sort -u > reading.list
