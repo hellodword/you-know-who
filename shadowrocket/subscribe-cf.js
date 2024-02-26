43dcaff2e6b21f2e67d937e696c1df0ed63f75ae
hellodword <46193371+hellodword@users.noreply.github.com>
2024-02-26 14:02:38 +0000

    update

--- shadowrocket/subscribe-cf.js
+++ shadowrocket/subscribe-cf.js
@@ -1,25 +1,84 @@
 export default {
     async fetch(req, env, ctx) {
+        const userAgent = req.headers.get("User-Agent") || "";
+        const isShadowRocket = userAgent.toLowerCase().indexOf("shadowrocket") != -1;
+
         const url = new URL(req.url);
-        const vType = url.searchParams.get("v_type");
+
         const vTag = url.searchParams.get("v_tag");
+        const vType = url.searchParams.get("v_type");
         const vUUID = url.searchParams.get("v_uuid");
         const vHost = url.searchParams.get("v_host");
         const vPath = url.searchParams.get("v_path");
         const vName = url.searchParams.get("v_name");
-        const vNetwork = url.searchParams.get("v_network");
+        const vHY2Pass = url.searchParams.get("v_hy2_pass");
+        const vHY2Port = url.searchParams.get("v_hy2_port");
 
-        if (!vTag || !vUUID || !vHost || !vPath || !vName || !vNetwork) {
+
+        const hosts = JSON.parse(env.DIRECT_HOSTS);
+
+        if (vType === 'direct-vmess') {
+
+            if (!isShadowRocket) {
+                return new Response("error");
+            }
+
+            if (!vTag || !vUUID || !vHost || !vPath || !vName) {
+                return new Response("error");
+            }
+
+            let outputs = [];
+
+            for (let host in hosts) {
+                const vmessName = `${vTag}:${vName}:cf:${hosts[host]}`;
+                const realHost = `${hosts[host].replace(/\./g, '-')}.${vHost}`;
+
+                outputs.push(`vmess://${btoa(`auto:${vUUID}@${host}:443`)}?remarks=${vmessName}&obfsParam=%7B%22Host%22:%22${realHost}%22%7D&path=${vPath}&obfs=websocket&tls=1&mux=1&alterId=0&sni=${vHost}`);
+            }
+
+            return new Response(btoa(outputs.join("\n")), {
+                headers: {
+                    "content-type": "text/html; charset=UTF-8",
+                }
+            });
+        }
+
+
+        if (vType === 'direct-hy2') {
+
+            if (!isShadowRocket) {
+                return new Response("error");
+            }
+
+            if (!vTag || !vHost || !vName || !vHY2Pass || !vHY2Port) {
+                return new Response("error");
+            }
+
+            let outputs = [];
+
+            for (let host in hosts) {
+                const hy2Name = `${vTag}:${vName}:hy2:${hosts[host]}`;
+                const realHost = `${hosts[host].replace(/\./g, '-')}.${vHost}`;
+
+                outputs.push(`hysteria2://${vHY2Pass}@${host}:${vHY2Port}?peer=${realHost}&obfs=none#${hy2Name}`)
+            }
+
+            return new Response(btoa(outputs.join("\n")), {
+                headers: {
+                    "content-type": "text/html; charset=UTF-8",
+                }
+            });
+        }
+
+        const vNetwork = url.searchParams.get("v_network");
+        const vNum = parseInt(url.searchParams.get("v_num"));
+
+        if (!vTag || !vUUID || !vHost || !vPath || !vName || !vNetwork || vNum <= 0 || vNum > 5) {
             return new Response("error");
         }
 
-        const userAgent = req.headers.get("User-Agent") || "";
-        const isShadowRocket = userAgent.toLowerCase().indexOf("shadowrocket") != -1;
-
-        let outputs = [];
-
-
         const fetchData = async (_type) => {
+            let outputs = [];
             try {
                 const response = await fetch('https://api.hostmonit.com/get_optimization_ip', {
                     method: 'POST',
@@ -36,6 +95,7 @@ export default {
                         } else {
                             outputs.push(`vmess://${btoa(`{"add":"${item.ip}","aid":"0","alpn":"","fp":"","host":"${vHost}","id":"${vUUID}","net":"ws","path":"${vPath}","port":"443","ps":"${name}","scy":"aes-128-gcm","sni":"${vHost}","tls":"tls","type":"","v":"2"}`)}`);
                         }
+
                     });
                 } else {
                     console.error('Error:', response.statusText);
@@ -43,17 +103,21 @@ export default {
             } catch (error) {
                 console.error('Error:', error);
             }
+            return outputs;
         };
 
+        let outputs4 = [];
+        let outputs6 = [];
+
         if (vType !== "6") {
-            await fetchData("v4");
+            outputs4 = await fetchData("v4");
         }
 
         if (vType !== "4") {
-            await fetchData("v6");
+            outputs6 = await fetchData("v6");
         }
 
-        return new Response(btoa(outputs.join("\n")), {
+        return new Response(btoa(outputs4.slice(0, vNum).concat(outputs6.slice(0, vNum)).join("\n")), {
             headers: {
                 "content-type": "text/html; charset=UTF-8",
             }
